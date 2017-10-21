a549e8b0c07d1681a46ce4bcda67420425a6f1d8
Kevin Tran <kevin.tran@bellevuecollege.edu>
2017-10-21 00:02:03 -0700

    Add files via upload

---
+++ Airline_Tracker/cmake-build-debug/CMakeFiles/Airline_Tracker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Airline_Tracker.dir/Launcher.cpp.o"
+  "CMakeFiles/Airline_Tracker.dir/OrderedLinkedList.cpp.o"
+  "Airline_Tracker.pdb"
+  "Airline_Tracker.exe"
+  "libAirline_Tracker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Airline_Tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
